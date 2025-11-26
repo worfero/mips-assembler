02fc56452333939a5364eeb57a994949020d4519
Henrique Wolf <henriquewolf1999@gmail.com>
2025-11-26 16:41:25 -0300

    separate a file for parser functions, for better organization

--- include/mips.h
+++ include/mips.h
@@ -1,76 +1,11 @@
 #ifndef MIPS_H
 #define MIPS_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <ctype.h>
 
+#include "parser.h"
 #include "utils.h"
 #include "file_manager.h"
-
-#define R_TYPE                  1           // R-TYPE instruction
-#define I_TYPE                  2           // I-TYPE instruction
-#define J_TYPE                  3           // J-TYPE instruction
-#define N_A                     0           // field not applicable
-#define INPUT_FIELD             10          // input field
-#define INVALID_INSTRUCTION     99          // invalid instruction
-#define INVALID_REGISTER        99          // invalid register
-
-#define MOVE                    0           // MOVE pseudo-instruction index
-#define LI                      1           // LI pseudo-instruction index
-
-#define MAX_REG_NUM             31          // Maximum number of registers available
-#define MAX_OPCODE_NUM          56          // Maximum number of registers available
-#define MAX_LABELS              100         // Maximum number of labels provided
-
-typedef unsigned char byte;
-
-typedef struct {
-    unsigned index;
-    char mnemonic[10];
-} Label;
-
-typedef struct {
-    // declaring instruction variables
-    char mnemonic[20];
-    byte type;
-    byte op;
-    byte rd;
-    byte rs;
-    byte rt;
-    short imm;
-    byte sa;
-    byte funct;
-} Instruction;
-
-typedef struct {
-    char mnemonic[20];
-    byte argCount;
-    bool isTwoInstructions;
-} PseudoInstruction;
-
-// defining register structure
-typedef struct {
-    char mnemonic[6]; // mnemonic
-    char alt_mne[6]; // alternate mnemonic
-    byte numCode; // opcode number
-} Register;
-
-void storeLabels(char **codeLines, unsigned *numberOfLines);
-
-byte getRegister(char *regMne);
-
-void getDefaultParams(char *opmne, Instruction *inst);
-
-void rTypeParsing(char *msg, Instruction *parsedInst);
-
-void iTypeParsing(char *msg, Instruction *parsedInst, unsigned index);
-
-void jTypeParsing(char *msg, Instruction *parsedInst);
-
-void instructionParsing(char *msg, unsigned index, Instruction *cur_inst);
 
 unsigned generateInstruction(Instruction inst);
 
---
+++ include/parser.h
@@ -0,0 +1,75 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "utils.h"
+
+#define MAX_REG_NUM             31          // Maximum number of registers available
+#define MAX_OPCODE_NUM          56          // Maximum number of registers available
+#define MAX_LABELS              100         // Maximum number of labels provided
+
+#define MOVE                    0           // MOVE pseudo-instruction index
+#define LI                      1           // LI pseudo-instruction index
+
+#define R_TYPE                  1           // R-TYPE instruction
+#define I_TYPE                  2           // I-TYPE instruction
+#define J_TYPE                  3           // J-TYPE instruction
+#define N_A                     0           // field not applicable
+#define INPUT_FIELD             10          // input field
+#define INVALID_INSTRUCTION     99          // invalid instruction
+#define INVALID_REGISTER        99          // invalid register
+
+#define BUF_SIZE_LINE           100         // Maximum buffer for a line
+
+typedef unsigned char byte;
+
+typedef struct {
+    unsigned index;
+    char mnemonic[10];
+} Label;
+
+typedef struct {
+    // declaring instruction variables
+    char mnemonic[20];
+    byte type;
+    byte op;
+    byte rd;
+    byte rs;
+    byte rt;
+    short imm;
+    byte sa;
+    byte funct;
+} Instruction;
+
+typedef struct {
+    char mnemonic[20];
+    byte argCount;
+    bool isTwoInstructions;
+} PseudoInstruction;
+
+// defining register structure
+typedef struct {
+    char mnemonic[6]; // mnemonic
+    char alt_mne[6]; // alternate mnemonic
+    byte numCode; // opcode number
+} Register;
+
+void storeLabels(char **codeLines, unsigned *numberOfLines);
+
+byte getRegister(char *regMne);
+
+void getDefaultParams(char *opmne, Instruction *inst);
+
+void rTypeParsing(char *msg, Instruction *parsedInst);
+
+void iTypeParsing(char *msg, Instruction *parsedInst, unsigned index);
+
+void jTypeParsing(char *msg, Instruction *parsedInst);
+
+void instructionParsing(char *msg, unsigned index, Instruction *cur_inst);
+
+#endif
